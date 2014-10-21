dc326f5526187b50dde2620bb75e1161f23079e6
wduntak <wduntak@gmail.com>
2014-10-21 15:16:12 -0400

    Created an add function that will continue to add until quit

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,40 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+#Adding items to the list
+
+isValid = false
+
+until isValid
+
+	puts "What would you like to add to the grocery list? (Type 'quit' to quit.)"
+
+	item_to_add = gets.chomp.downcase
+
+	if item_to_add == "quit"
+
+		puts "Closing..."
+
+		isValid = true
+
+	else
+		grocery_list << "#{item_to_add}"
+
+		puts "#{item_to_add} has been added."
+
+		isValid = false
+
+	end
+end
+
+#Add * to all of the items in array
+
 grocery_list.map! {|x| "*" + x}
+
+
+#Output list
+
+puts " "
+
+puts "Here's your grocery list:"
 
 puts grocery_list
