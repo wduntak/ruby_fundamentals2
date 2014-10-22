d40b874b4df18b0fb01fab825405ec2889efe9f5
wduntak <wduntak@gmail.com>
2014-10-22 12:26:11 -0400

    Add cohort4 with 43 students to Hash

--- exercise7.rb
+++ exercise7.rb
@@ -4,4 +4,12 @@ students = {
 	:cohort3 => 22
 }
 
+#Display each cohort name and amount of students
+
 students.each{|key, value| puts "#{key} : #{value}"}
+
+#Add 43 students for cohort 4
+
+students.store("cohort4", 43)
+
+puts students
